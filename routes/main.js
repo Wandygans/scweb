fded727ad746a4f4105402d1a4ff7a21f233250e
Wandygans <87267241+Wandygans@users.noreply.github.com>
2022-10-12 17:05:02 +0700

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -10,7 +10,7 @@ router.get('/page', (req, res) => {
 res.sendFile(__path + '/view/index.html')
 })
 router.get('/tiktok', (req, res) => {
-res.sendFile(__path + '/publc/tiktok.html')
+res.sendFile(__path + '/public/tiktok.html')
 })
 router.get('/game.html', (req, res) => {
 res.sendFile(__path + '/view/game.html')
